e1ad8be01357764979aeba2046d13d093aa4a641
jannis Daum <jannis.daum@rwth-aachen.de>
2026-08-10 17:05:03 +0200

    optimizer init

--- optimization/optimizer.py
+++ optimization/optimizer.py
@@ -2,11 +2,16 @@ import numpy as np
 import pandas as pd
 import pyomo.environ as pyo
 
+from data_collection.prices import Prices
+
 
 class Optimizer():
     objects = {}
 
-    def __init__(self):
+    def __init__(self, import_prices: Prices = None, export_prices: Prices = None):
+        self.import_prices = import_prices
+        self.export_prices = export_prices
+        
         self.model = pyo.ConcreteModel()
 
     @staticmethod
