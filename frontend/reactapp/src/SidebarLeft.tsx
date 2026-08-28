8d68927c3a3c8220023484ab904cb33441a12ae0
Jannis Daum <jannisdaum1@gmail.com>
2026-08-29 00:04:10 +0200

    mobile deploy 1.0

--- frontend/reactapp/src/SidebarLeft.tsx
+++ frontend/reactapp/src/SidebarLeft.tsx
@@ -10,25 +10,31 @@ type AssetData = {
 };
 type AssetIconProps = {
     name: string,
-    data: AssetData
+    data: AssetData,
+    onAddAsset: (name: string) => void,
 }
 
-function AssetIcon({name, data}: AssetIconProps){
+function AssetIcon({name, data, onAddAsset}: AssetIconProps){
     const image_path = data.image;
     return (
         <div className='AssetIcon' key={name} 
         draggable onDragStart={(e) => e.dataTransfer.setData("asset", name)}>
             <img src={image_path ?? ""} alt={name} draggable={false}/>
             <p>{name}</p>
+            <button type="button" onClick={() => onAddAsset(name)}>Hinzufügen</button>
         </div>
     )
 }
-function SidebarLeft() {
+type SidebarLeftProps = {
+    onAddAsset: (name: string) => void,
+};
+
+function SidebarLeft({onAddAsset}: SidebarLeftProps) {
     return (
         <div className="SideBarLeft">
             {Object.entries(assets).map(([name, data]) => (
                 !excludeAssets.includes(name) &&
-                <AssetIcon name={name} data={data}></AssetIcon>
+                <AssetIcon name={name} data={data} onAddAsset={onAddAsset}></AssetIcon>
             ))}
         </div>
     );
